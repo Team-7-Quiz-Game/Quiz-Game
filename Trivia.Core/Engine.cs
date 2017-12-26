8db24269096919486207e552284e3deeb4258de8
Plamen Atanasov <plamen.atanasov91@gmail.com>
2017-12-26 16:28:04 +0200

    more engine logic

--- Trivia.Core/Engine.cs
+++ Trivia.Core/Engine.cs
@@ -14,12 +14,12 @@ namespace Trivia.Core
         private static readonly IEngine SingleInstance = new Engine();
         private readonly IFactory factory;
         private static IPlayer player;
-        private readonly IList<ICategory> categories;
+        private readonly IDictionary<string, ICategory> categories;
         
         private Engine()
         {
             this.factory = new Factory();
-            this.categories = new List<ICategory>();
+            this.categories = new Dictionary<string, ICategory>();
         }
 
         public static IEngine Instance
@@ -32,108 +32,118 @@ namespace Trivia.Core
 
         public static IPlayer Player { get => player; private set => player = value; }
 
-        public void Start()
+        //public void Start()
+        //{
+        //    while (true)
+        //    {
+        //        try
+        //        {
+        //            //var commandAsString = this.Reader.ReadLine();
+
+        //            //if (commandAsString.ToLower() == TerminationCommand.ToLower())
+        //            //{
+        //            //    break;
+        //            //}
+
+        //            //this.ProcessCommand(commandAsString);
+        //        }
+        //        catch (Exception ex)
+        //        {
+        //            //this.Writer.WriteLine(ex.Message);
+        //            //this.Writer.WriteLine("####################");
+        //        }
+        //    }
+        //}
+
+        //private void ProcessCommand(string commandAsString)
+        //{
+        //    if (string.IsNullOrWhiteSpace(commandAsString))
+        //    {
+        //        throw new ArgumentNullException("Command cannot be null or empty.");
+        //    }
+
+        //    //var command = this.Parser.ParseCommand(commandAsString);
+        //    //var parameters = this.Parser.ParseParameters(commandAsString);
+
+        //    //var executionResult = command.Execute(parameters);
+        //    //this.Writer.WriteLine(executionResult);
+        //    //this.Writer.WriteLine("####################");
+        //}
+
+        public void CreateCategory(IList<string> categories)
         {
-            while (true)
+            for (int i = 0; i < categories.Count; i++)
             {
-                try
-                {
-                    //var commandAsString = this.Reader.ReadLine();
+                var categoryName = categories[i];
 
-                    //if (commandAsString.ToLower() == TerminationCommand.ToLower())
-                    //{
-                    //    break;
-                    //}
-
-                    //this.ProcessCommand(commandAsString);
-                }
-                catch (Exception ex)
+                if (this.categories.ContainsKey(categoryName))
                 {
-                    //this.Writer.WriteLine(ex.Message);
-                    //this.Writer.WriteLine("####################");
+                    //throw some custom error or something - category already exists
                 }
+
+                var categoryType = (CategoryType)Enum.Parse(typeof(CategoryType), categories[i]);
+
+                var categoryToAdd = this.factory.CreateCategory(categoryType);
+                this.categories.Add(categoryName, categoryToAdd);
             }
         }
 
-        private void ProcessCommand(string commandAsString)
+        public void AddQuestionToCategory(string categoryNameToAdd, IQuestion questionToAdd)
         {
-            if (string.IsNullOrWhiteSpace(commandAsString))
+            if (!this.categories.ContainsKey(categoryNameToAdd))
             {
-                throw new ArgumentNullException("Command cannot be null or empty.");
+                //throw some custom error or something - category does not exists
             }
 
-            //var command = this.Parser.ParseCommand(commandAsString);
-            //var parameters = this.Parser.ParseParameters(commandAsString);
+            var category = this.categories[categoryNameToAdd];
 
-            //var executionResult = command.Execute(parameters);
-            //this.Writer.WriteLine(executionResult);
-            //this.Writer.WriteLine("####################");
+            switch (questionToAdd.DifficultyLevel)
+            {
+                case DifficultyLevel.Easy:
+                    category.AddEasyQuestion(questionToAdd);
+                    break;
+                case DifficultyLevel.Normal:
+                    category.AddNormalQuestion(questionToAdd);
+                    break;
+                case DifficultyLevel.Hard:
+                    category.AddHardQuestion(questionToAdd);
+                    break;
+                default:// throw some error
+                    break;
+            }
         }
 
-        private void CreateCategory(string categoryName)
-        {
-            //if (this.categories.Contains(categoryName))
-            //{
-            //    return string.Format(CategoryExists, categoryName);
-            //}
-
-            //var category = this.factory.CreateCategory(categoryName);
-            //this.categories.Add(categoryName, category);
-
-            //return string.Format(CategoryCreated, categoryName);
-        }
-
-        private void AddQuestionToCategory(string categoryNameToAdd, IQuestion questionToAdd)
-        {
-            //if (!this.categories.ContainsKey(categoryNameToAdd))
-            //{
-            //    return string.Format(CategoryDoesNotExist, categoryNameToAdd);
-            //}
-
-            //if (!this.products.ContainsKey(productToAdd))
-            //{
-            //    return string.Format(ProductDoesNotExist, productToAdd);
-            //}
-
-            //var category = this.categories[categoryNameToAdd];
-            //var product = this.products[productToAdd];
-
-            //category.AddProduct(product);
-
-            //return string.Format(ProductAddedToCategory, productToAdd, categoryNameToAdd);
-        }
-
-        private IAnswer CreateAnswer(string answerText, bool isCorrect)
+        public IAnswer CreateAnswer(string answerText, bool isCorrect)
         {
             //gurad
             return this.factory.CreateAnswer(answerText, isCorrect);
         }
 
-        private IQuestion CreateNormalQuestion(string questionText, DifficultyLevel difficultyLevel, CategoryType category)
+        public IQuestion CreateNormalQuestion(string questionText, DifficultyLevel difficultyLevel, CategoryType category)
         {
             //guard
             return this.factory.CreateNormalQuestion(questionText, difficultyLevel, category);
         }
 
-        private IQuestion CreateBonusQuestion(string questionText, DifficultyLevel difficultyLevel, CategoryType category, int pointsAmplifier)
+        public IQuestion CreateBonusQuestion(string questionText, DifficultyLevel difficultyLevel, CategoryType category, int pointsAmplifier)
         {
             //guard
             return this.factory.CreateBonusQuestion(questionText, difficultyLevel, category, pointsAmplifier);
         }
 
-        private IQuestion CreateTimedQuestion(string questionText, DifficultyLevel difficultyLevel, CategoryType category, int timeForAnswer)
+        public IQuestion CreateTimedQuestion(string questionText, DifficultyLevel difficultyLevel, CategoryType category, int timeForAnswer)
         {
             //guard
             return this.factory.CreateTimedQuestion(questionText, difficultyLevel, category, timeForAnswer);
         }
 
-        private IPlayer CreateNormalPlayer(string name)
+        public IPlayer CreateNormalPlayer(string name)
         {
             //guard
             return this.factory.CreateNormalPlayer(name);
         }
 
-        private IPlayer CreateQuizzardPlayer(string name)
+        public IPlayer CreateQuizzardPlayer(string name)
         {
             //guard
             return this.factory.CreateQuizzardPlayer(name);
