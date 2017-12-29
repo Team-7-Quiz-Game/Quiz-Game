443ccb4e933c7b4b805cfefe176828f5b81d6565
Plamen Atanasov <plamen.atanasov91@gmail.com>
2017-12-29 14:38:44 +0200

    added engine and db get random questions logic

--- Trivia/Contracts/ICategory.cs
+++ Trivia/Contracts/ICategory.cs
@@ -1,10 +1,17 @@
-﻿using Trivia.Common;
+﻿using System.Collections.Generic;
+using Trivia.Common;
 
 namespace Trivia.Contracts
 {
     public interface ICategory
     {
         CategoryType CategoryType { get; }
+
+        IList<IQuestion> EasyQuestions { get; }
+
+        IList<IQuestion> NormalQuestions { get; }
+
+        IList<IQuestion> HardQuestions { get; }
 
         void AddQuestion(IQuestion question);
     }
