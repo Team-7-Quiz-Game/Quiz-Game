8ee3e6770f94d5713acec18156c7fe1d9687b5c4
Plamen Atanasov <plamen.atanasov91@gmail.com>
2018-02-13 00:10:36 +0200

    Added more test; Refactored and fixed tests; Removed unnecessary folders; Added task requirements;

---
+++ ProjectRequirementsHQC&DP.md
@@ -0,0 +1,66 @@
+# C\# High Quality Code & Design Patterns Teamwork Assignment - .NET Alpha Nov'2017
+
+## Project Description
+
+Refactor and test the object-orientated application that you have developed during the previous course.
+
+## General Requirements
+
+Please define and implement the following in your project:
+
+- The **OOP** principles (where applicable)
+  - Inheritance
+  - Encapsulation
+  - Abstraction
+  - Polymorphism
+
+- The **SOLID** principles (where applicable)
+  - Single responsibility
+  - Open/closed
+  - Liskov substitution
+  - Interface segregation
+  - Dependency inversion
+
+- Dependency inversion container (using Autofac)
+  - Favor composition over inheritance
+  - Introduce object lifetime management
+  - Introduce convention based binding
+
+- Unit tests
+  - Unisolated tests will be looked upon as **invalid**!
+  - Introduce **at least 25** unit tests that use **both**:
+     - **MSTest** (or other testing framework)
+     - **Moq** (or other mocking framework)
+
+## Additional Requirements
+
+- Follow the **best practices for OO design**: use data encapsulation, use exception handling properly, abstraction and polymorphism properly, follow the principles of strong cohesion and loose coupling
+- Follow the **best practices for SOLID design**
+- Obligatory use Git to keep your source code and for team collaboration (you might use https://github.com/)
+- Provide a class diagram (to visualize all types)
+
+## Non-Required Work
+
+- Completely finished project is not obligatory required. It will not be a big problem if your project is not completely finished or is not working greatly. This team work project is for educational purpose. Its main purpose it to experience object-oriented modeling and OOP in a real-world project and to get some experience in team working and team collaboration with Git.
+
+## Deliverables
+
+- The complete source code.
+- Brief documentation of your project in **.md**(markdown). It should provide the following information (in brief):
+  - Team name and list of team members (with http://my.telerikacademy.com usernames)
+  - What problems did the application have before you started refactoring?
+  - What design patterns have you used and why?
+  - Class diagram of your types
+  - Any other information (optionally)
+
+## Project Defense
+
+Each team will have to deliver a public defense of its work in front of the trainers. You will have around 20 minutes for the following:
+
+- Demonstrate the changed you have introduced during the refactoring
+- Show the source code in the Git web-based source code browser.
+- Show the commits logs to confirm that each team member has contributed.
+
+## Give Feedback about Your Teammates
+
+You will be invited to provide feedback about all your teammates, their attitude to this project, their technical skills, their team working skills, their contribution to the project, etc. The feedback is important part of the project evaluation so **take it seriously and be honest**.
---
+++ Trivia.Tests/ModelsTests/QuestionTests.cs
@@ -0,0 +1,127 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using System.Collections.Generic;
+using Trivia.Common.Enums;
+using Trivia.Contracts;
+using Trivia.Models.Question;
+
+namespace Trivia.Tests.Models
+{
+    [TestClass]
+    public class QuestionTests
+    {
+        [TestCategory("BonusQuestionTests")]
+        [TestMethod]
+        public void ConstructorShouldReturnSameTextForQuestionWhenGivenValidParameters()
+        {
+            var nameQuestion = "Bonus";
+            var bonusQuestion = new BonusQuestion(nameQuestion, DifficultyLevel.Hard, CategoryType.Geography, 5);
+            Assert.AreEqual("Bonus", bonusQuestion.QuestionText);
+        }
+
+        [TestCategory("BonusQuestionTests")]
+        [TestMethod]
+        public void ConstructorShouldReturnProperlyCalculatedPointsBasedOnFormula()
+        {
+            var pointsMultiplier = 5;
+            var bonusQuestion = new BonusQuestion("Bonus", DifficultyLevel.Normal, CategoryType.Geography, pointsMultiplier);
+            var pointsAccordingToFormula = (int)DifficultyLevel.Normal * 100 * pointsMultiplier;
+
+            Assert.AreEqual(pointsAccordingToFormula, bonusQuestion.Points);
+        }
+
+        [TestCategory("NormalQuestionTests")]
+        [TestMethod]
+        public void ConstructorShouldReturnProperlyCalculatedPointsForNormalQuestionBasedOnFormula()
+        {
+            var normalQuestion = new NormalQuestion("Normal", DifficultyLevel.Hard, CategoryType.Movies, QuestionType.Normal);
+            var pointsCalculatedByFormula = (int)DifficultyLevel.Hard * 100;
+
+            Assert.AreEqual(pointsCalculatedByFormula, normalQuestion.Points);
+        }
+        
+        [TestCategory("QuestionTests")]
+        [TestMethod]
+        public void AddAnswerFluentShouldAddToTheNormalQuestionAnswerFluently()
+        {
+            var answerMock = new Mock<IAnswer>();
+            var text = "NormalQuestionText";
+
+            var question = new NormalQuestion(text, DifficultyLevel.Easy, CategoryType.Geography, QuestionType.Normal);
+
+            question.AddAnswerFluent(answerMock.Object)
+                .AddAnswerFluent(answerMock.Object)
+                .AddAnswerFluent(answerMock.Object)
+                .AddAnswerFluent(answerMock.Object);
+
+            Assert.IsTrue(question.Answers.Count == 4);
+        }
+
+        [TestCategory("QuestionTests")]
+        [TestMethod]
+        public void ShuffleAnswersShouldShuffleTheAnswers()
+        {
+            var answerMock = new Mock<IAnswer>();
+            var answerMock2 = new Mock<IAnswer>();
+            var answerMock3 = new Mock<IAnswer>();
+            var answerMock4 = new Mock<IAnswer>();
+            var text = "NormalQuestionText";
+
+            var question = new NormalQuestion(text, DifficultyLevel.Easy, CategoryType.Geography, QuestionType.Normal);
+
+            question.AddAnswerFluent(answerMock.Object)
+                .AddAnswerFluent(answerMock2.Object)
+                .AddAnswerFluent(answerMock3.Object)
+                .AddAnswerFluent(answerMock4.Object);
+
+            question.ShuffleAnswers();
+
+            Assert.IsTrue(question.Answers[0] != answerMock.Object ||
+                question.Answers[1] != answerMock2.Object ||
+                question.Answers[2] != answerMock3.Object ||
+                question.Answers[3] != answerMock4.Object);
+        }
+
+        [TestCategory("TimedQuestionTests")]
+        [TestMethod]
+        public void ConstructorShouldCreateValidTimedQuestionWhenValidParametersAreProvided()
+        {
+            var text = "QuestionText";
+            var question = new TimedQuestion(text, DifficultyLevel.Easy, CategoryType.Geography, 10);
+
+            Assert.IsInstanceOfType(question, typeof(IQuestion));
+        }
+
+        [TestCategory("TimedQuestionTests")]
+        [TestMethod]
+        public void TimedQuestionConstructorShouldThorwArgumentOutOfRangeExceptionWhenTimeBelowTheLimitIsProvided()
+        {
+            var text = "QuestionText";
+
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => new TimedQuestion(text, DifficultyLevel.Easy, CategoryType.Geography, -10));
+        }
+
+
+        [TestCategory("TimedQuestionTests")]
+        [TestMethod]
+        public void TimedQuestionConstructorShouldThorwArgumentOutOfRangeExceptionWhenTimeAboveTheLimitIsProvided()
+        {
+            var text = "QuestionText";
+
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => new TimedQuestion(text, DifficultyLevel.Easy, CategoryType.Geography, 30));
+        }
+
+        [TestCategory("TimedQuestionTests")]
+        [TestMethod]
+        public void TimedQuestionTimeGetterShouldCorrectTimeWhenValidParametersAreProvidedToTheConstructor()
+        {
+            var text = "QuestionText";
+            var time = 10;
+
+            var question = new TimedQuestion(text, DifficultyLevel.Easy, CategoryType.Geography, time);
+
+            Assert.AreEqual(time, question.Time);
+        }
+    }
+}
